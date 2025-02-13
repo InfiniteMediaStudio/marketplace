e5af46993da7acd3532f90c538fd5ce1410fec39
InfiniteMediaStudio <infinitemedia92@gmail.com>
2025-02-13 20:23:34 +0400

    first push

---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  // Configuration for the Astro project
+  devToolbar: { enabled: false }, // Disable the development toolbar
+});
